f300a11cbdc0da37cde253eefd59b97c9a799c19
Kanik0575 <f20230575@pilani.bits-pilani.ac.in>
2026-04-01 02:16:31 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-# Ensure project root is on the path
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.corpus_loader       import CorpusLoader
